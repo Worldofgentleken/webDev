b565e7ef68f9e8f3f4b9c70d1a444df340673f02
Worldofgentleken <djman8787@gmail.com>
2024-07-03 11:59:25 +0900

    modified:   source code/study/javascript/01-basic/ex07-objects.js

---
+++ source code/study/javascript/01-basic/ex08-typeof.js
@@ -0,0 +1,15 @@
+let a = "홍길동";
+let b = 3.14;
+let c = true;
+let d = [1, 2, 3];
+let e = {x:1, y:2};
+let f = undefined;
+let g = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
