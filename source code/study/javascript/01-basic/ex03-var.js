c881825c88b5752edef8abbb1e399ffab4942230
Worldofgentleken <djman8787@gmail.com>
2024-07-03 11:34:53 +0900

    modified:   source code/study/javascript/01-basic/ex03-var.js

--- source code/study/javascript/01-basic/ex03-var.js
+++ source code/study/javascript/01-basic/ex03-var.js
@@ -3,3 +3,15 @@
     상수: 메모리의 공간
     자료형: 변수, 상수 데이터 형식(차지하는 공간의 크기)    
 */
+
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+
+var x = 7;
+z = x + y;
+console.log(z);
+
+var n = 10;
+console.log(n);
