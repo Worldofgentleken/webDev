912dde6e415d1ef666229de45544b607fa9f9f71
Worldofgentleken <djman8787@gmail.com>
2024-07-17 13:24:51 +0900

    modified:   .DS_Store

---
+++ source_code/study/react/source_code/example/src/App01.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ex01Hello from './ex01-jsx/Ex01Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Ex01Hello who="홍길동" />
+      <h1>나왔지롱</h1>
+    </div>
+  );
+}
+
+export default App;
