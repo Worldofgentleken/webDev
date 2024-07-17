49700e99d346380f4748873448eb18e60d9f34de
Worldofgentleken <djman8787@gmail.com>
2024-07-17 16:45:03 +0900

    modified:   source_code/study/react/source_code/example/src/App02.js

--- source_code/study/react/source_code/example/src/App02.js
+++ source_code/study/react/source_code/example/src/App02.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './ex02-elements/ParentComponent';
+import Tick from './ex02-elements/Tick';
 
 function App() {
   return (
     <div className="App">
       <ParentComponent />
+      <hr></hr>
+      <Tick />
     </div>
   );
 }
