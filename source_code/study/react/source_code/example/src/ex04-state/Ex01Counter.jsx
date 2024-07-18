3ba3ed483af65d53c50d7d06e70e874e26effdac
Worldofgentleken <djman8787@gmail.com>
2024-07-18 11:36:25 +0900

    new file:   source_code/study/react/source_code/example/src/ex04-state/Ex02LikeButton.jsx

--- source_code/study/react/source_code/example/src/ex04-state/Ex01Counter.jsx
+++ source_code/study/react/source_code/example/src/ex04-state/Ex01Counter.jsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function Ex01Counter(){
-    const [count, setCount] = useState(0);
-    let cnt = 0;
+  console.log('카운터 렌더링');
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(10);
 
-    const increment = () => {
-        setCount(count + 1);
-        cnt++;
-        console.log('count', count);
-        console.log('cnt', cnt);
-    }
-    return (
-        <div>
-            <h1>카운트 : {count}</h1>
-            <button onClick={increment}>증가</button>
-        </div>
-    );
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>카운트: {count}</h1>
+      <button onClick={increment}>증가</button>
+      <h1>카운트1: {count1}</h1>
+      <button onClick={() => setCount1(count1 + 1)}>증가</button>
+    </div>
+  );
 }
 
 export default Ex01Counter;
