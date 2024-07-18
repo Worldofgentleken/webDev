445a84e60049718c370e18224b376a7c04bdcce3
Worldofgentleken <djman8787@gmail.com>
2024-07-18 12:28:06 +0900

    modified:   source_code/study/react/source_code/example/src/App04.js

--- source_code/study/react/source_code/example/src/App04.js
+++ source_code/study/react/source_code/example/src/App04.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Ex01Counter from './ex04-state/Ex01Counter';
+import Ex03BadCounter from './ex04-state/Ex03BadCounter';
+import Ex02LikeButton from './ex04-state/Ex02LikeButton';
 
 function App() {
   return (
     <div className="App">
       <Ex01Counter />
+      <Ex02LikeButton />
+      <Ex03BadCounter />
     </div>
   );
 }
