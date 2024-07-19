67a850e481211cdffdf70de401cc32021a92931f
Worldofgentleken <djman8787@gmail.com>
2024-07-19 12:48:23 +0900

    modified:   source_code/study/react/source_code/example/src/App06.js

--- source_code/study/react/source_code/example/src/App06.js
+++ source_code/study/react/source_code/example/src/App06.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Ex01useState from './ex06-hooks/Ex01useState';
 import Ex02useEffect from './ex06-hooks/Ex02useEffect';
+import Ex03useRef from './ex06-hooks/Ex03useRefVariable';
+import Ex03useRefDOM from './ex06-hooks/Ex03useRefDOM';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React!!</h1>
-      <Ex01useState/>
-      <Ex02useEffect />
+      {/* <Ex01useState/> */}
+      {/* <Ex02useEffect /> */}
+      {/* <Ex03useRef /> */}
+      <Ex03useRefDOM />
     </div>
   );
 }
