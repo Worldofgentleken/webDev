22d17ce564df105404d43cc8c8e0e1a4ed4cfd73
Worldofgentleken <djman8787@gmail.com>
2024-07-19 14:37:31 +0900

    modified:   source_code/study/react/source_code/example/src/App06.js

--- source_code/study/react/source_code/example/src/App06.js
+++ source_code/study/react/source_code/example/src/App06.js
@@ -4,6 +4,7 @@ import Ex01useState from './ex06-hooks/Ex01useState';
 import Ex02useEffect from './ex06-hooks/Ex02useEffect';
 import Ex03useRef from './ex06-hooks/Ex03useRefVariable';
 import Ex03useRefDOM from './ex06-hooks/Ex03useRefDOM';
+import Ex04useContextParent from './ex06-hooks/Ex04useContextParent';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Ex01useState/> */}
       {/* <Ex02useEffect /> */}
       {/* <Ex03useRef /> */}
-      <Ex03useRefDOM />
+      {/* <Ex03useRefDOM /> */}
+      <Ex04useContextParent />
     </div>
   );
 }
