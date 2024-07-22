730b15992047bac5fd9f9a2d94e67ef9e3f642ff
Worldofgentleken <djman8787@gmail.com>
2024-07-22 12:37:14 +0900

    modified:   source_code/study/node_js/.DS_Store

--- source_code/study/node_js/ex02-built-in/ex05-__dirname.js
+++ source_code/study/node_js/ex02-built-in/ex05-__dirname.js
@@ -1,3 +1,9 @@
+/*
+__dirname
+  현재 모듈의 디렉토리 경로
+  현재 실행 중인 스크립트 파일이 위치한 디렉토리의 절대 경로를 제공
+*/
+
 console.log(__dirname);
 
 const path = require('path');
--- source_code/study/node_js/ex02-built-in/ex07_URL.js
+++
@@ -1,7 +0,0 @@
-const myurl = new URL('https://www.google.com:8080/search?q=javascript&user=tester#bottom');
-
-console.log(myurl.hostname);
-console.log(myurl.port);
-console.log(myurl.pathname);
-console.log(myurl.search);
-console.log(myurl.hash);
