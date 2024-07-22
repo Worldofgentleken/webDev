c5ea24bdecd0d4d83e50c97e97bc148601980580
Worldofgentleken <djman8787@gmail.com>
2024-07-22 12:04:03 +0900

    new file:   source_code/study/node_js/ex02-built-in/ex08-req-resq.js

---
+++ source_code/study/node_js/ex02-built-in/ex08-req-resq.js
@@ -0,0 +1,21 @@
+
+const request = new Request('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: "POST",
+        body: JSON.stringify({
+            title: '홍',
+            body: '길동',
+            userID: 1
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=utf-8'
+        }
+    }
+);
+
+console.log(request);
+console.log('\n');
+fetch(request)
+    .then(response => {
+        console.log(response);
+    })
