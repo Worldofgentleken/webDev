7d42c7cd9a03e9e461a730416603c41f6090613c
Worldofgentleken <djman8787@gmail.com>
2024-08-02 19:19:38 +0900

    modified:   source_code/study/react_study/source_code/react-app/src/App.js

--- source_code/study/react_study/source_code/react-app/src/App.js
+++ source_code/study/react_study/source_code/react-app/src/App.js
@@ -1,30 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+
 function Header(props) {
-  console.log('props', props.title)
+  console.log("props", props.title);
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 }
@@ -38,12 +56,167 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" value={title} onChange={event =>{
+            console.log(event.target.value);
+            setTitle(event.target.value);
+          }} />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body" value={body} onChange={event => {
+            console.log(event.target.value);
+            setBody(event.target.value);
+          }}></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Update"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
+  const [mode, setMode] = useState("Welcome");
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ]);
+  let content = null;
+  let contextControl = null;
+  if (mode === "Welcome") {
+    content = <Article title="Welcome" body="Hello, Web"></Article>;
+  } else if (mode === "Read") {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+    contextControl = <>
+    <li><a href={"/update"*id} onClick={event =>{
+      event.preventDefault();
+      setMode("Update");
+    }}>Update</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics = []
+      for(let i=0; i<topics.length; i++){
+        if(topics[i].id === id){
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode("Welcome");
+    }} /></li></>
+  } else if (mode === "Create") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode("Read");
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
+  } else if (mode === "Update"){
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    content = <Update title={title} body={body} onUpdate={(title, body) =>{
+      console.log(title, body);
+      const newTopics = [...topics];
+      const updateTopics = {id:id, title:title, body:body}
+      for(let i = 0; i<topics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updateTopics;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode("Read");
+    }}></Update>
+  }}
+
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav></Nav>
-      <Article title="Welcome" body="Hello, web"></Article>
+      <Header
+        title="WEB"
+        onChangeMode={() => {
+          setMode("Welcome");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("Read");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
+      <ul>
+        <li>
+          <a href="/create"
+            onClick={(event) => {
+              event.preventDefault();
+              setMode("Create");
+            }}>
+            Create
+          </a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
