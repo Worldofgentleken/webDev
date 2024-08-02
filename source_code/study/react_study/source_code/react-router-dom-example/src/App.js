93846fcee2e3a31590c837cd63af82bd2a40cc91
Worldofgentleken <djman8787@gmail.com>
2024-08-02 19:36:42 +0900

    modified:   .DS_Store

---
+++ source_code/study/react_study/source_code/react-router-dom-example/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home(){
+  return(
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+}
+
+function Topics() {
+  return(
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return(
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+function App(){
+  return(
+    <div>
+      <h1>Hello React Router DOM</h1>
+    <Home></Home>
+    <Topics></Topics>
+    <Contact></Contact>
+    </div>
+  );
+}
+
+export default App;
